7b9cafc2f30509e71e5fe8365f73f3a5ca7e95ff
Hifzu04 <hifzurbtech@gmail.com>
2024-08-31 15:12:50 +0530

    done

--- A/alternatingSumOfNumbers.cpp
+++ A/alternatingSumOfNumbers.cpp
@@ -6,8 +6,25 @@ int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    
+    ll testcases;
+    cin >> testcases;
 
+    for (ll i = 0; i < testcases; i++)
+    {
+        ll size;
+        cin >> size;
+        ll arr[size];
+        ll sum = 0;
+        for (ll i = 0; i < size; i++)
+        {
+            cin >> arr[i];
+            if (i % 2 == 0)
+                sum += arr[i];
+            else
+                sum -= arr[i];
+        }
+        cout << sum << endl;
+    }
 
     return 0;
 }
