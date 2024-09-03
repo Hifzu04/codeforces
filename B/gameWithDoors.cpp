5d0e7c469dbe2f34c23f8dc230be358ef19cd8e6
Hifzu04 <hifzurbtech@gmail.com>
2024-09-03 20:19:00 +0530

    runs on all testcases

--- B/gameWithDoors.cpp
+++ B/gameWithDoors.cpp
@@ -5,28 +5,14 @@ using namespace std;
 int main()
 {
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+    cin.tie(NULL);~
     ll t;
     cin >> t;
     while (t--)
     {
-        int a,b,c,d;
-        cin>>a>>b>>c>>d;
-        if(b<c) {
-            cout<<1<<"\n";
-            continue;
-        }
-        ll  l_common = max(a,c);
-        ll r_common = min(b,d);
-        ll ext_left = min(a,c);
-        ll ext_right = max(b,d);
-        ll sum = (r_common-l_common);
-        if(ext_left<l_common) sum++;
-        if (r_common<ext_right) sum++;
-
-        cout<<sum<<"\n";
-
-
+        int a, b, c, d;
+        cin >> a >> b >> c >> d;
+        cout << max(min(b, d) - max(a, c) + (b != d) + (a != c), 1) << "\n";
     }
 
     return 0;
