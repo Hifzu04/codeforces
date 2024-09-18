b25616f078ce1f5b9a0c916f014e04918d77b8be
Hifzu04 <hifzurbtech@gmail.com>
2024-09-18 23:40:03 +0530

    done

--- Codeforces Round 969 (Div. 2)/B.Index and Maximum Value.cpp
+++ Codeforces Round 969 (Div. 2)/B.Index and Maximum Value.cpp
@@ -8,42 +8,71 @@ int main()
     cin.tie(NULL);
     int t;
     cin >> t;
-    while (t--)
-    {
-        int n, m;
-        cin >> n >> m;
-        int arr[n] = {0};
-        for (int i = 0; i < n; i++)
-        {
-            cin >> arr[i];
-        }
-        char c;
-        int l, r;
+    // while (t--)
+    // {
+    //     int n, m;
+    //     cin >> n >> m;
+    //     int arr[n] = {0};
+        
+    //     for (int i = 0; i < n; i++)
+    //     {
+    //         cin >> arr[i];
+    //     }
+    //     char c;
+    //     int l, r;
 
-        for (int i = 0; i < m; i++)
-        {
-            cin >> c >> l >> r;
-            for (int i = 0; i < n; i++)
-            {
-                if (c == '+' and arr[i] <= r and arr[i] >= l)
-                    arr[i] += 1;
-            }
-            for (int i = 0; i < n; i++)
-            {
-                if (c == '-' and arr[i] <= r and arr[i] >= l)
-                {
-                    arr[i] -= 1;
-                }
-            }
-            int maxm = -1;
-            for (int i = 0; i < n; i++)
-            {
-                maxm = max(arr[i], maxm);
-            }
-            cout << maxm << " ";
+    //     for (int i = 0; i < m; i++)
+    //     {
+    //         cin >> c >> l >> r;
+    //         for (int i = 0; i < n; i++)
+    //         {
+    //             if (c == '+' and arr[i] <= r and arr[i] >= l)
+    //                 arr[i] += 1;
+    //         }
+    //         for (int i = 0; i < n; i++)
+    //         {
+    //             if (c == '-' and arr[i] <= r and arr[i] >= l)
+    //             {
+    //                 arr[i] -= 1;
+    //             }
+    //         }
+    //         int maxm = -1;
+    //         for (int i = 0; i < n; i++)
+    //         {
+    //             maxm = max(arr[i], maxm);
+    //         }
+    //         cout << maxm << " ";
            
+    //     }
+    //     cout << endl;
+    //}
+
+    while (t--){
+       
+       int n , m ;
+       cin>>n>>m;
+       int maxm = -1;
+       int arr[n];
+       for(int i = 0 ; i< n ; i++){
+        cin>>arr[i];
+        maxm = max(maxm, arr[i]);
+       }
+        for(int i=0; i<m ; i++){
+            char c; 
+            cin>>c;
+            int l , r;
+            cin>>l>>r;
+            
+         if(maxm <= r and maxm >= l){
+             if(c =='+') maxm += 1;
+             if(c== '-') maxm -= 1;
+          } 
+          cout<<maxm <<" ";
+        
         }
-        cout << endl;
+        cout<<"\n";
+
+
     }
 
     return 0;
