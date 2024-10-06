b8af0ecf83d3c879ceb81f0953bf320012a05f37
Hifzu04 <hifzurbtech@gmail.com>
2024-10-06 11:18:51 +0530

    done

--- Codeforces Round 974 (Div. 3)/C.Robin Hood in Town.cpp
+++ Codeforces Round 974 (Div. 3)/C.Robin Hood in Town.cpp
@@ -20,26 +20,47 @@ int main()
             sum += vec[i];
         }
         sort(vec.begin(), vec.end());
-        ll mid_idx = n / 2;
-        ll half_avg = sum / (n * 2);
+        //ll mid_idx = n / 2;
+        // ll half_avg = sum / (n * 2);
         
-        //auto it =  lower_bound(vec.begin(), vec.end(), half_avg);
-        ///cout << *(it)<< endl;
-        if(vec.size()<3){
+        // //auto it =  lower_bound(vec.begin(), vec.end(), half_avg);
+        // ///cout << *(it)<< endl;
+        // if(vec.size()<3){
+        //     cout<<-1<<"\n";
+        // }
+        //  else if(vec[mid_idx] < half_avg)
+        //  {
+        //      cout << 0 << "\n";
+        //  }
+        //  else
+        //  {
+        //      ll ans = (vec[mid_idx] * 2 * n) - sum + 1;
+        //      if (ans < 0)
+        //          cout << -1 << "\n";
+        //      else
+        //          cout << ans << "\n";
+        //  }
+        ll ans = 0;
+         if(vec.size()<3 ){
             cout<<-1<<"\n";
-        }
-         else if(vec[mid_idx] < half_avg)
-         {
-             cout << 0 << "\n";
-         }
-         else
-         {
-             ll ans = (vec[mid_idx] * 2 * n) - sum + 1;
-             if (ans < 0)
-                 cout << -1 << "\n";
-             else
-                 cout << ans << "\n";
-         }
+
+         } 
+         else{
+           ll j= n/2;
+         ans =(vec[j]*2*n)-sum;
+           if(ans<0)
+            cout<<0<<"\n";
+         else cout<<ans+1<<"\n";
+
+           
+
+           
+
+         } 
+            
+        
+
+
     }
 
     return 0;
