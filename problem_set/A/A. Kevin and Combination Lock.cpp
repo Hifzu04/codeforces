d5c0c57bdc490207c1d4ab6b57ec36ebeedf9e36
Hifzu04 <hifzurbtech@gmail.com>
2025-01-13 21:45:01 +0530

    done

---
+++ problem_set/A/A. Kevin and Combination Lock.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define loop(n) for(int i=0;i<n;i++)
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t; cin>>t; 
+    while(t--){    
+    ll x; cin>>x;
+    if(x%33==0) cout<<"YES"<<"\n";
+    else cout<<"NO"<<"\n";
+
+
+
+}
+    return 0;
+}
