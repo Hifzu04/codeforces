158602c5728f150b6151da749ac91d710c0e4f80
Hifzu04 <hifzurbtech@gmail.com>
2025-06-04 17:16:21 +0530

    done

---
+++ problem_set/Div 3/C/C. Need More Arrays.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using ll = long long;
+#define loop for (ll i = 0; i < n; i++)
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    ll arr[n];
+    loop cin >> arr[i];
+
+    ll ans = 1;
+    ll cur = arr[0];
+    for (ll i = 1; i < n; i++)
+    {
+        if (arr[i] - cur > 1)
+        {
+            ans++;
+            cur =arr[i];
+        }
+    }
+    cout<<ans<<"\n";
+}
+int main()
+{
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
